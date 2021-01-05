cd564e07d69d4b194a698daff61745fe4501e9ce
TheyKay <murraykyleb@gmail.com>
2021-01-04 21:04:42 -0700

    updating Footer.jsx to get it to show up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,13 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <>
-      <div id="foot">
-        {/* <p style={{paddingBottom: "0", marginBottom: "0"}}>&#169; Kay Murray</p>
+    <div id="foot">
+      {/* <p style={{paddingBottom: "0", marginBottom: "0"}}>&#169; Kay Murray</p>
         <p style={{fontSize: ".5rem", marginTop: "0", paddingTop: "0"}}>This is The Kay</p> */}
-        <p>&#169; Kay Murray</p>
-        <p>This is The Kay</p>
-      </div>
-    </>
+      <p>&#169; Kay Murray</p>
+      <p>This is The Kay</p>
+    </div>
+
   )
 }
 
