80412fe5b9a87d927ca77eae0131ae9fb9b88715
TheyKay <murraykyleb@gmail.com>
2021-01-04 21:21:37 -0700

    more attempts to fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div id="foot">
-      {/* <p style={{paddingBottom: "0", marginBottom: "0"}}>&#169; Kay Murray</p>
-        <p style={{fontSize: ".5rem", marginTop: "0", paddingTop: "0"}}>This is The Kay</p> */}
-      <p>&#169; Kay Murray</p>
-      <p>This is The Kay</p>
+      <p style={{ paddingBottom: "0", marginBottom: "0" }}>&#169; Kay Murray</p>
+      <p style={{ fontSize: ".5rem", marginTop: "0", paddingTop: "0" }}>This is The Kay</p>
     </div>
 
   )
